c018a9238f46c2ac734e01566e37ecb758156273
mofumofuchan <you_oki1@yahoo.co.jp>
2021-04-25 23:24:09 +0900

    ファイルを一行ずつ読むコードを追加

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,26 @@
-
-fn func1(a: &i32) -> i32{
-    return a*2;
-}
+use std::error::Error;
+use std::fs::File;
+use std::env;
+use std::io::prelude::*;
+use std::io::BufReader;
+use std::path::Path;
 
 fn main() {
-    let s = String::from("hello");
-    let t = String::from("hello");
-    let u = String::from("hello");
+    let args: Vec<String> = env::args().collect();
 
-    let v = vec![s, t, u];
-    println!("{}", v[0]);
+    let filename = &args[1];
+    let path = Path::new(filename);
+    let display = path.display();
+
+    let f = match File::open(&path) {
+        Err(why) => panic!("couldn't open {}: {}",
+                           display, why.description()),
+        Ok(file) => file,
+    };
+
+    let reader = BufReader::new(f);
+
+    for line in reader.lines() {
+        println!("> {}", line.unwrap());
+    }
 }
